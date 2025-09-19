0a4b6a8bc0d6a1fb383db56fa23724bec62dc53d
doodadada <121083215+doodadada@users.noreply.github.com>
2025-09-19 16:52:20 +0900

    프론트 구조 수정
    자세한 내용 fn_structure.md 파일참조

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import MainLayout from './page/MainLayout';
-import LandingPage from './page/LandingPage';
-import HairCheck from './page/HairCheck';
-import SignUp from './user/SignUp';
-import LogIn from './user/LogIn';
-import BaspSelfCheck from './features/selfcheck/BaspSelfCheck';
-import HairDamageAnalysis from './page/HairDamageAnalysis';
-import HairChange from './page/HairChange';
-import HairPT from './components/HairPT';
-import HairLossProducts from './page/HairLossProducts';
-import AiToolList from './page/AiToolList';
-import MainContent from './page/MainContent';
-import YouTubeVideos from './page/YouTubeVideos';
-import HairEncyclopediaMain from './features/hairEncyclopedia/HairEncyclopediaMain';
-import HairDiagnosis from './components/HairDiagnosis';
-import HairQuiz from './components/HairQuiz';
-import DailyCare from './page/DailyCare';
+import MainLayout from './pages/MainLayout';
+import LandingPage from './pages/LandingPage';
+import HairCheck from './pages/check/HairCheck';
+import SignUp from './pages/users/SignUp';
+import LogIn from './pages/users/LogIn';
+import HairDamageAnalysis from './pages/check/HairDamageAnalysis';
+import HairChange from './pages/hair_contents/HairChange';
+import HairPT from './pages/hair_solutions/HairPT';
+import HairLossProducts from './pages/hair_solutions/HairLossProducts';
+import AiToolList from './pages/AiToolList';
+import MainContent from './pages/MainContent';
+import YouTubeVideos from './pages/hair_contents/YouTubeVideos';
+import HairEncyclopediaMain from './pages/hair_contents/hairEncyclopedia/HairEncyclopediaMain';
+import HairDiagnosis from './pages/check/HairDiagnosis';
+import HairQuiz from './pages/hair_contents/HairQuiz';  
+import DailyCare from './pages/hair_solutions/DailyCare';
+import LandingPageV2 from './pages/LandingPageV2';
 
 // TypeScript: React 함수형 컴포넌트 타입 정의
 const App: React.FC = () => {
@@ -25,7 +25,8 @@ const App: React.FC = () => {
     <>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<LandingPage />} />
+          <Route index element={<LandingPageV2 />} />
+          {/* <Route index element={<LandingPage />} /> */}
           <Route path="main" element={<MainContent />} />
           <Route path="hair-check" element={<HairCheck />} />
           <Route path="hair-damage" element={<HairDamageAnalysis />} />
@@ -33,7 +34,6 @@ const App: React.FC = () => {
           <Route path="hair-pt" element={<HairPT />} />
           <Route path="hair-loss-products" element={<HairLossProducts />} />
           <Route path="youtube-videos" element={<YouTubeVideos />} />
-          <Route path="basp-check" element={<BaspSelfCheck />} />
           <Route path="ai-tools" element={<AiToolList />} />
           <Route path="product-search" element={<HairLossProducts />} />
           <Route path="hair-encyclopedia/*" element={<HairEncyclopediaMain />} />
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,8 +4,8 @@ import './index.css'; // Tailwind CSS import 추가
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'; 
-import { store, persistor } from './store/store';
-import apiClient from './api/apiClient';
+import { store, persistor } from './utils/store';
+import apiClient from './services/apiClient';
 import { PersistGate } from 'redux-persist/integration/react';
 
 // TypeScript: 함수 타입 정의
