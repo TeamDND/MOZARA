952bf0ff57cd8c1e88b2fb053d48ae1abb1bc4c8
jojeongtae <jeongtae9324@gmail.com>
2025-10-13 16:18:16 +0900

    frontend oauth2

--- frontend/src/pages/users/OAuth2Proxy.tsx
+++ frontend/src/pages/users/OAuth2Proxy.tsx
@@ -8,7 +8,7 @@ const OAuth2Proxy: React.FC = () => {
     console.log('=== OAuth2Proxy 컴포넌트 렌더링됨 ===');
     console.log('백엔드 OAuth2 엔드포인트로 리다이렉트 시작');
     // 백엔드 OAuth2 엔드포인트로 리다이렉트
-    window.location.href = 'http://hairfit.duckdns.org:8080/oauth2/authorization/google';
+    window.location.href = '/oauth2/authorization/google';
   }, []);
 
   return (
